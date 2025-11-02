5ea77491ee8ace5f4eadbf09daeda5fb1a308a61
amrhgold <paypal26205162@gmail.com>
2025-11-02 12:19:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,4 +31,5 @@ def webhook():
         return {'code': 'ignored', 'message': 'No valid signal'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+
